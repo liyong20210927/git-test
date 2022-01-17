43b5616bd1b612fd642832fa778dfdc8ebd0d393
liyong20210927 <91444384+liyong20210927@users.noreply.github.com>
2022-01-17 15:53:38 +0800

    pull commit

--- src/main/java/com/ly/Git.java
+++ src/main/java/com/ly/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("hello git4! master test");
         System.out.println("hello git4 hot-fix test!");
         System.out.println("push test!");
+                System.out.println("pull test!");
     }
 }
